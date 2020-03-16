c26bbe33337bdf52a3782f749ee2a79f4dee3234
ciemekk92 <przemyslaw.reducha@gmail.com>
2020-03-16 11:45:18 +0100

    Production touches.

--- src/components/Result/Image/Image.js
+++ src/components/Result/Image/Image.js
@@ -3,21 +3,21 @@ import classes from './Image.module.css';
 import styled from 'styled-components';
 
 const Image = props => {
-   const ParagraphType = styled.p`
-      margin: 20px 0 0 5px;
-      font-size: 10px;
-      text-align: left;
-   `;
-   return (
-      <div className={classes.ImageBox}>
-         <ParagraphType>{props.type}</ParagraphType>
-         <img
-            className={classes.Image}
-            src={props.imageSource}
-            alt={props.alt}
-         />
-      </div>
-   );
+    const ParagraphType = styled.p`
+        margin: 20px 0 0 5px;
+        font-size: 10px;
+        text-align: left;
+    `;
+    return (
+        <div className={classes.ImageBox}>
+            <ParagraphType>{props.type}</ParagraphType>
+            <img
+                className={classes.Image}
+                src={props.imageSource}
+                alt={props.alt}
+            />
+        </div>
+    );
 };
 
 export default Image;
