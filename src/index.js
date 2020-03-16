a3a459c6e81c1d158b2a12aba4f9938d935245a3
ciemekk92 <przemyslaw.reducha@gmail.com>
2020-03-16 15:28:55 +0100

    Changed default font to be more inline with logo-style.

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import './fonts/Pixel Digivolve.otf';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware, compose, combineReducers } from 'redux';
 import thunk from 'redux-thunk';
