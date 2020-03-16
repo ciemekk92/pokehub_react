f11a891113803c9b6375d50bd8b3e09d06d59d5b
ciemekk92 <przemyslaw.reducha@gmail.com>
2020-03-16 15:42:56 +0100

    Corrected mobile styling

--- src/containers/Metrics/Metrics.js
+++ src/containers/Metrics/Metrics.js
@@ -4,20 +4,20 @@ import classes from './Metrics.module.css';
 import DataBox from '../../components/UI/DataBox/DataBox';
 
 const Metrics = props => {
-   return (
-      <div className={classes.Container}>
-         <DataBox type="Height" value={props.height / 10 + ' m'} />
-         <DataBox type="Weight" value={props.weight / 10 + ' kg'} />
-         <DataBox type="Base Experience" value={props.baseExperience} />
-      </div>
-   );
+    return (
+        <div className={classes.Container}>
+            <DataBox type="Height" value={props.height / 10 + ' m'} />
+            <DataBox type="Weight" value={props.weight / 10 + ' kg'} />
+            <DataBox type="B. Experience" value={props.baseExperience} />
+        </div>
+    );
 };
 
 const mapStateToProps = state => {
-   return {
-      height: state.result.height,
-      weight: state.result.weight,
-      baseExperience: state.result.baseExperience
-   };
+    return {
+        height: state.result.height,
+        weight: state.result.weight,
+        baseExperience: state.result.baseExperience
+    };
 };
 export default connect(mapStateToProps)(Metrics);
--- src/containers/Stats/Stats.js
+++ src/containers/Stats/Stats.js
@@ -4,11 +4,20 @@ import { capitalizeFirstLetter } from '../../shared/utility';
 import StatBox from '../../components/UI/StatBox/StatBox';
 
 const Stats = props => {
+    const shortenType = input => {
+        if (input === 'special-attack') {
+            return 'Sp-ATK';
+        } else if (input === 'special-defense') {
+            return 'Sp-DEF';
+        } else {
+            return input;
+        }
+    };
     return props.stats
         .map(element => (
             <StatBox
-                key={element.stat.name}
-                type={capitalizeFirstLetter(element.stat.name)}
+                key={capitalizeFirstLetter(element.stat.name)}
+                type={capitalizeFirstLetter(shortenType(element.stat.name))}
                 value={element.base_stat}
             />
         ))
